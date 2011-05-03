49abb5ef6b0bf76390c4147ee392982be9db8d69
Admin <Admin@5766e509-19ac-44c2-bbb0-941299f47fb9>
2011-05-03 12:11:26 +0000

    added support for query strings
    git-svn-id: svn://10.0.1.208:4000/silverstripe/modules/mouseketeers/colorbox/trunk@853 5766e509-19ac-44c2-bbb0-941299f47fb9

--- Code/Colorbox.php
+++ Code/Colorbox.php
@@ -6,10 +6,19 @@ class Colorbox extends DataObjectDecorator {
 	static $selectors = array(
 		'colorbox' => 'maxWidth:"90%",maxHeight:"90%",scalePhotos:true,current: "Image {current} of {total}"',
 		'colorboxIframe' => 'width:"600", innerHeight:"500", iframe:true',
-		'colorboxPage' => 'href: function() { return $(this).attr("href")+"/colorboxpage"},width:"600px",height:"500px"'
+		'colorboxPage' => 'href: function() {
+				var href = $(this).attr("href");
+				var query = "";
+				queryStart = href.indexOf("?")
+				if(queryStart > -1) {
+					var query = href.substring(queryStart);
+					var href = href.substring(0, queryStart);
+				}
+				return href+"/colorboxpage/"+query
+			},width:"600px",height:"500px"'
 	);
 	static $template;
-	public function index() {
+	public function init() {
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery-packed.js');
 		Requirements::javascript('colorbox/javascript/jquery.colorbox-min.js');
 		$selectors_js = '';
